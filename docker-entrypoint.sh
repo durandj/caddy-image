c723bd4f3e71a4395236c9742195929f7bb5c276
James Durand <james@durand.zone>
2026-07-24 10:43:51 -0500

    Added OIDC_SIGNING_KEY secret for JWT's

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,5 +23,6 @@ function load_secret_if_set() {
 load_secret_if_set "CLOUDFLARE_API_TOKEN" "${CLOUDFLARE_API_TOKEN_FILE:-}"
 load_secret_if_set "OIDC_CLIENT_ID" "${OIDC_CLIENT_ID_FILE:-}"
 load_secret_if_set "OIDC_CLIENT_SECRET" "${OIDC_CLIENT_SECRET_FILE:-}"
+load_secret_if_set "OIDC_SIGNING_KEY" "${OIDC_SIGNING_KEY_FILE:-}"
 
 exec "${ENTRYPOINT}" "$@"
